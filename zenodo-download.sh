7b59df682ccdf1b3998df8c81bfa9ff3f06b9935
aprovoNYU <ap180@nyu.edu>
2026-02-02 15:22:43 -0500

    update notebooks

--- zenodo-download.sh
+++ zenodo-download.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 filename=$1
+
 tail -n +2 "$filename" | while read line; do
-	zenodo_get $line -o $line -g *.xls
+	zenodo_get $line -g "*.csv" -g "*.tsv" -g "*.xls" -g "*.xlsb" -g "*.xlsx" -g "*.xltx" -g "*.ods" -g "*.ots" -g "*.xlsm" -g "*.xltm" -g "*.xltx" -g "*.ogw" -o $line
 
 done <"$filename"
